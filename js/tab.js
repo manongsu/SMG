76b83264067d0efcaa15c8d80ccb3b0d2d8e9e3c
苏鹏飞 <15735298718@163.com>
2017-05-22 18:09:40 +0800

    番茄网-05.22 首页，新闻列表、详情，视频列表、详情，抢票，边走边晒列表、详情，电子阅读，分机查询，联系方式，短号查询，集团短号查询，邮箱查询，查询结果，宣传-播音支持人园地、视听研究，团购

--- js/tab.js
+++ js/tab.js
@@ -3,9 +3,21 @@ function tab($tabLi,tabWrap) {
     $tabLi.addClass("active").siblings("li").removeClass("active");
     $(tabWrap).eq(index).css("display","block").siblings(tabWrap).css("display","none");
 }
+<!--JS计算absolute自动撑开-->
+function absoluteHeight($tabWrap) {
+    $tabWrap.each(function () {
+        if($(this).css("display")=="block"){
+            var height = $(this).css("height");
+            $(".message").css("padding-bottom",height);
+        }
+    });
+}
 $(".tab li").click(function () {
     tab($(this),".tabWrap");
+    absoluteHeight($(".tabWrap"));
 });
+
 $(function () {
     tab($(".tab li").eq(0),".tabWrap");
+    absoluteHeight($(".tabWrap"));
 });
