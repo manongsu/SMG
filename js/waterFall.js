6734a61e014d370b66992bf261627828dd36df0e
manongsu <manongsu@163.com>
2017-07-23 18:21:46 +0800

    瀑布流性能优化

--- js/waterFall.js
+++ js/waterFall.js
@@ -1,5 +1,5 @@
 
-$(function () {
+$(".waterFall ").load(function () {
     function waterFall() {
         var height1 = 0,height2 = 0,height3 = 0;
         var mB = parseInt($("body").css("font-size"))/2;
